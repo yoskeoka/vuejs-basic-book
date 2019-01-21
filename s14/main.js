df8ec636d67ce3b39fb504b8ebe254c74a1684b6
yoskeoka <y.okamoto@gemcook.com>
2019-01-21 09:27:14 +0900

    [add] section 14

--- s14/main.js
+++ s14/main.js
@@ -1,7 +1,16 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'hello world!',
-    val: [],
+    width: 800,
+  },
+  computed: {
+    halfWidth: {
+      get() {
+        return this.width / 2;
+      },
+      set(val) {
+        this.width = val * 2;
+      },
+    },
   },
 });
